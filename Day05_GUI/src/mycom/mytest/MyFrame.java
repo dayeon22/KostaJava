b2460e06e9c2ed1db314cfaabf21724e7bc37f99
Dayeon Park <ddggs@naver.com>
2023-04-11 11:00:43 +0900

    Day05

--- Day05_GUI/src/mycom/mytest/MyFrame.java
+++ Day05_GUI/src/mycom/mytest/MyFrame.java
@@ -1,24 +1,39 @@
 package mycom.mytest;
 
-import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 
 //도형이 그려진 Frame
 public class MyFrame extends Frame { //상속의 장점: 남이 짜놓은 코드를 일부분만 바꿔서 내 것처럼 사용 가능.
 	
+	Player[] arr = new Player[10]; //배열선언문. arr은 player 10개짜리 주소를 가리키고, arr[i]는 null을 가리킴.
+	
 	public MyFrame() {
 		super(); //외부 클래스에서 사용 시: Frame f = new Frame();
+		makePlayers();
 		this.setSize(800, 600); //f.setSize(800, 600);
-		this.setVisible(true);
+		this.setVisible(true);		
+	}
+
+	//일단 잘 돌아가면 효율성을 위해 함수를 분리하는 등 코드를 변경함(리펙토링)
+	private void makePlayers() {
+		if (arr != null) { //안정적인 코드를 위해서는 null guard가 필요함.(null은 exception으로 처리하는 것 아님! if로 처리하기)
+			for (int i = 0; i < arr.length; i++) { //자바에서는 배열을 객체화해서 다루기 때문에 length라는 속성을 사용 가능.
+				arr[i] = new Player();
+			}
+		}
 	}
 	
 	@Override
 	public void paint(Graphics g) { //시그널이 발생하여 paint()가 불릴 때 수행(내가 부르는 함수가 아님)
-		for (int i = 0; i < 10; i++) {
-			Player pobj = new Player();
-			pobj.display(g);
-		}
+		if (arr != null) {
+			for (Player item : arr) {
+				//객체는 자신이 모든 일을 해야 함(player가 자체적으로 그림을 그릴 수 있게 해야 함)
+				if (item != null) {
+					item.display(g); //그림을 그릴 수 있는 Graphics 객체를 전달해 줌 -> display 함수에서 g를 사용할 수 있으므로 그림 그리기 가능
+				}
+			} //for
+		} //if
 		
 	}
 	
--- Day05_GUI/src/mycom/mytest/Player.java
+++ Day05_GUI/src/mycom/mytest/Player.java
@@ -3,7 +3,8 @@ package mycom.mytest;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
-
+/*
+//내 코드
 public class Player {
 	private int x;
 	private int y;
@@ -21,9 +22,9 @@ public class Player {
 		Random random = new Random();
 		this.x = random.nextInt(800);
 		this.y = random.nextInt(600);
-		this.r = random.nextInt(255);
-		this.g = random.nextInt(255);
-		this.b = random.nextInt(255);
+		this.r = random.nextInt(256);
+		this.g = random.nextInt(256);
+		this.b = random.nextInt(256);
 		this.width = width;
 		this.height= height;
 	}
@@ -41,4 +42,32 @@ public class Player {
         graphics.fillPolygon(starX, starY, 10);
 	}
 
+}
+*/
+
+//강사님 코드
+public class Player {
+	private int x; //인스턴스 변수(인스턴스마다 가지는 변수)
+	private int y;
+	public static final int shapeSize = 50; //클래스 변수(모든 인스턴스가 공유하는 변수). 보통 선언문에서 명시적 초기화를 한다.
+	//static변수는 메모리할당이 가장 먼저 이루어지기 때문에, New연산자로 객체생성 여부와 관계없이 접근가능.
+	//Player.shapeSize로 접근한다.
+	//어차피 final이니까 public으로 열어 둠.
+	
+	public Player() {
+		Random rnd = new Random();
+		//화면 너비가 800이라고 하면 양 옆 50픽셀정도는 가려져있다. 도형이 잘리지 않게 하기 위해 양 옆 50픽셀(총 100픽셀) 정도는 여유로 남겨둠.
+		x = rnd.nextInt(800 - 100) + 50; //0 ~ (최댓값 - 1)
+		y = rnd.nextInt(600 - 140) + 70; // 70 ~ 529 픽셀
+	}
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void display(Graphics g) {
+		g.setColor(new Color(255, 0, 0));
+		g.fillOval(x, y, Player.shapeSize, Player.shapeSize);
+	}
 }
