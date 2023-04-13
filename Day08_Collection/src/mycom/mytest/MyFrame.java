5bfa36f7032e86d9d75ae311b386755a55c5e1e0
Dayeon Park <ddggs@naver.com>
2023-04-13 17:04:12 +0900

    singleton

--- Day08_Collection/src/mycom/mytest/MyFrame.java
+++ Day08_Collection/src/mycom/mytest/MyFrame.java
@@ -11,7 +11,8 @@ import java.util.Random;
 public class MyFrame extends Frame {
 	public static final int FRAME_WIDTH = 800;
 	public static final int FRAME_HEIGHT = 600;
-	ArrayList<MyShape> arr = new ArrayList<MyShape>();
+
+	public MyShapeManager shapeMgr = MyShapeManager.getInstance();
 	
 	public MyFrame() {
 		super("마우스 클릭하면 도형이 생성되는 윈도우");
@@ -23,38 +24,19 @@ public class MyFrame extends Frame {
 	@Override
 	public void paint(Graphics g) {
 		//도형그리기
-		for (MyShape shape : arr) {
-			shape.display(g);
-		}
+		shapeMgr.drawShapes(g);
 	}
 
 //	inner class: outer에서 접근을 많이하거나 잠깐 쓰고 버릴 때 사용(?)
 	//MyListener가 mouseListener를 상속받고, 여기서는 MyListener를 상속받아 원하는 함수만 override할 수 있음
 	class MyHandler extends MyListener {
 		
-		public MyShape RandomShape(int x, int y) {
-			Random rnd = new Random();
-			int type = rnd.nextInt(4);
-			
-			switch(type) {
-			case 0:
-				return new MyCircle(x, y);
-			case 1:
-				return new MyRect(x, y);
-			case 2:
-				return new MyStar(x, y);
-			case 3:
-				return new MyTri(x, y);
-			default:
-				return new MyCircle(x, y);
-			}
-		}
-		
 		@Override
 		public void mousePressed(MouseEvent e) { //e에 이벤트정보(마우스 좌표 등 모든정보)들어있음
 			int mx = e.getX() - (MyShape.SHAPE_SIZE / 2);
 			int my = e.getY() - (MyShape.SHAPE_SIZE / 2);
-			MyFrame.this.arr.add(RandomShape(mx, my)); //그냥 arr.add() 해도 되지만, 충돌시에 outer class의 arr에 접근하는 문법
+//			MyFrame.this.arr.add(RandomShape(mx, my)); //그냥 arr.add() 해도 되지만, 충돌시에 outer class의 arr에 접근하는 문법
+			shapeMgr.addShapes(mx, my);
 			repaint(); //paint(Graphics g)는 우리가 호출하는 함수가 아님. g도 없음.
 		}
 		
--- Day08_Collection/src/mycom/mytest/MyShapeManager.java
+++ Day08_Collection/src/mycom/mytest/MyShapeManager.java
@@ -1,5 +1,52 @@
 package mycom.mytest;
 
-public class MyShapeManager {
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
 
+public class MyShapeManager {
+	ArrayList<MyShape> arr = new ArrayList<MyShape>();
+	private static MyShapeManager shapeMgr = new MyShapeManager();
+	
+	private MyShapeManager() {
+		
+	}
+
+	public static MyShapeManager getInstance() {
+		return shapeMgr;
+	}
+	
+	public void addShapes(int x, int y) {
+		Random rnd = new Random();
+		int type = rnd.nextInt(4);
+		
+		if (arr != null) {
+			switch(type) {
+			case 0:
+				arr.add(new MyCircle(x, y));
+				break;
+			case 1:
+				arr.add(new MyRect(x, y));
+				break;
+			case 2:
+				arr.add(new MyStar(x, y));
+				break;
+			case 3:
+				arr.add(new MyTri(x, y));
+				break;
+			default:
+				arr.add(new MyCircle(x, y));
+			}
+		}
+	}
+	
+	public void drawShapes(Graphics g) {
+		if (arr != null) {
+			for (MyShape shape : arr) {
+				if (shape != null) {
+					shape.display(g);
+				}
+			}
+		}
+	}
 }
